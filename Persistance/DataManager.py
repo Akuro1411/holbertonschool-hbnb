1628228e0a6c896d4d8be1186f0e9788f870a445
Akuro1411 <nahidisayev147@gmail.com>
2025-03-22 09:31:56 +0300

    DataManager is updated

--- Persistance/DataManager.py
+++ Persistance/DataManager.py
@@ -1,22 +1,24 @@
-import json
 from Persistance.IPersistance import IPersistenceManager
-from Model.Users import User
 
 
 class DataManager(IPersistenceManager):
     def __init__(self):
-        self.data_dict = {"Place": {}, "User": {}, "Amenity": {}, "Review": {}, "City": {}, "Country": {}}
+        self.data_dict = {"Place": {}, "User": {}, "Amenity": {}, "Reviews": {}, "City": {}, "Country": {}}
+
     def save(self, entity):
         entity_type = entity.__class__.__name__
         self.data_dict[entity_type][entity.object_id] = entity
         return f"{entity_type} is saved successfully"
 
-    def get(self, entity, entity_id):
-        entity_type = entity.__class__.__name__
+    def get(self, entity, entity_type):
+        entity_id = entity.object_id
         return f"Here you are: {self.data_dict[entity_type][entity_id]}"
 
+    def update(self, entity):
+        entity_type = entity.__class__.__name__
+        self.data_dict[entity_type][entity.object_id] = entity
+        return "Entity is updated successfully"
 
-new_user = User("Nahid", "Isayev", "nahid@gmail.com", "1234")
-data = DataManager()
-data.save(new_user)
-print(data.get(new_user, new_user.object_id))
+    def delete(self, entity_id, entity_type):
+        del self.data_dict[entity_type][entity_id]
+        return "Entity is deleted successfully"
