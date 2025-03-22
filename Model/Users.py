1fbee41452304cedef1afbca70d04483415ce95f
Akuro1411 <nahidisayev147@gmail.com>
2025-03-22 13:32:56 +0300

    Users is updated

--- Model/Users.py
+++ Model/Users.py
@@ -1,16 +1,13 @@
 from Model.Interface import Interface
-from validate_email import validate_email
+from Validation.UserValidation import email_validation
 
 
 class User(Interface):
-    def __init__(self, firstname, lastname, user_mail, password, owner=False):
+    def __init__(self, firstname, lastname, user_mail, password=None, owner=False):
         super().__init__()
         self.first_name = firstname
         self.last_name = lastname
-        if validate_email(user_mail):
-            self.email = user_mail
-        else:
-            raise ValueError("Email is not valid")
+        self.email = email_validation(user_mail)
         self.user_password = password
         self.is_owner = owner
         self.owned_places = []
